efc68806fe509ed6ae79d593c2b4a7cf78239a74
Vinod Pahuja <vinod.pahuja@gmail.com>
2024-02-19 14:19:53 +0530

    Initial import

---
+++ js/toolbox.js
@@ -0,0 +1,21 @@
+
+const toolbox = {
+  
+  kind: 'flyoutToolbox', contents: [
+  
+    { kind: 'block', type: 'map' },
+	{ kind: 'block', type: 'style' },
+	{ kind: 'block', type: 'source' },
+	{ kind: 'block', type: 'layer' },
+	
+	{ kind: 'block', type: 'sourceOSM' },
+	
+	{ kind: 'block', type: 'sourceESRISat' },
+	
+	{ kind: 'block', type: 'glyphESRI' },
+	{ kind: 'block', type: 'sourceESRI' },
+	
+	{ kind: 'block', type: 'layerESRIVector' },
+	{ kind: 'block', type: 'layerESRIHybrid' },
+  ]
+};
